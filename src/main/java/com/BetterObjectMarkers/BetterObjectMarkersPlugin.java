eb88e02f626ac7115a85f99fac9790c87df59d7f
JosteinE <Josteineri@hotmail.com>
2025-11-23 16:16:53 +0100

    initial setup

--- src/main/java/com/BetterObjectMarkers/BetterObjectMarkersPlugin.java
+++ src/main/java/com/BetterObjectMarkers/BetterObjectMarkersPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.BetterObjectMarkers;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -14,15 +14,15 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "Better Object Markers"
 )
-public class ExamplePlugin extends Plugin
+public class BetterObjectMarkersPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private BetterObjectMarkersConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+    BetterObjectMarkersConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(BetterObjectMarkersConfig.class);
 	}
 }
